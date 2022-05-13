c3bd369743be1c333ca9686ec167896356a71532
Yoav Lavi <yoav@grafbase.com>
2022-05-13 15:32:44 +0200

    await externally, errors

--- sentry-cf-worker/src/lib.rs
+++ sentry-cf-worker/src/lib.rs
@@ -1,4 +1,5 @@
 use sentry_core::{constants::USER_AGENT, IntoDsn};
+use thiserror::Error;
 use worker::{console_error, wasm_bindgen_futures};
 
 pub use sentry_core::protocol::*;
@@ -6,29 +7,37 @@ pub use sentry_core::Level;
 
 static SENTRY_AUTH_HEADER: &str = "X-Sentry-Auth";
 
-pub fn send_envelope(sentry_ingest_url: &str, envelope: Envelope) {
-    // FIXME
-    let dsn = sentry_ingest_url.clone().into_dsn().unwrap().unwrap();
+#[derive(Error)]
+pub enum SentryError {
+    #[error("invalid sentry ingest url")]
+    InvalidUrl,
+    #[error("the call to sentry returned an error")]
+    Request(Error),
+    #[error("could not write the given envelope")]
+    WriteEnvelope,
+}
 
-    wasm_bindgen_futures::spawn_local(async move {
-        let auth = dsn.to_auth(Some(&USER_AGENT)).to_string();
-        let url = dsn.envelope_api_url().to_string();
-        let mut body = Vec::new();
+pub async fn send_envelope(sentry_ingest_url: &str, envelope: Envelope) -> Result<(), SentryError> {
+    let dsn = sentry_ingest_url
+        .clone()
+        .into_dsn()
+        .map_err(|_| SentryError::InvalidUrl)?
+        .unwrap();
 
-        match envelope.to_writer(&mut body) {
-            Ok(_) => {}
-            Err(error) => console_error!("Error writing envelope: {}", error),
-        }
+    let auth = dsn.to_auth(Some(&USER_AGENT)).to_string();
+    let url = dsn.envelope_api_url().to_string();
+    let mut body = Vec::new();
 
-        let request = surf::Client::new()
-            .post(&url)
-            .header(SENTRY_AUTH_HEADER, &auth)
-            .body(body)
-            .await;
+    envelope
+        .to_writer(&mut body)
+        .map_err(|_| SentryError::WriteEnvelope)?;
 
-        match request {
-            Ok(_) => {}
-            Err(error) => console_error!("Request error: {}", error),
-        }
-    });
+    let _request = surf::Client::new()
+        .post(&url)
+        .header(SENTRY_AUTH_HEADER, &auth)
+        .body(body)
+        .await
+        .map_err(|error| SentryError::Request(error));
+
+    Ok(())
 }
